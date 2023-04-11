8625eb07f3b58a5218385de14802861a680014eb
Alessandro Piccione <66700181+Sandrgar@users.noreply.github.com>
2023-04-11 15:13:26 +0200

    Add files via upload

---
+++ Exercises 17 - Classes inheritance/exercise-2.js
@@ -0,0 +1,20 @@
+class Person {
+  firstName;
+  lastName;
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+}
+
+class Developer extends Person {
+  constructor(firstName, lastName, role){
+  super(firstName, lastName, role);
+  this.role = role;
+  }
+}
+
+const developer = new Developer("Mario", "Rossi", "Front-end");
+console.log(
+  developer.firstName + " " + developer.lastName + " " + developer.role
+);
